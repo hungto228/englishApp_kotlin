72cfe89634e5ee1662ff2b0771c3afcf0659990e
hungto228@gmail.com <hungto228@gmail.com>
2021-06-11 15:36:43 +0700

    create project

---
+++ app/src/main/java/com/hungto/english_kotlin/activity/IntroScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.hungto.english_kotlin.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hungto.english_kotlin.R
+
+class IntroScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro_screen)
+    }
+}
